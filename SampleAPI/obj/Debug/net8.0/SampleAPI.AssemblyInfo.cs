292df7063a46d6a6c70bf7233d970c03a8561af7
Dhiraj <dhirajbirnale384@gmail.com>
2025-03-25 00:18:18 +0530

    my change

--- SampleAPI/obj/Debug/net8.0/SampleAPI.AssemblyInfo.cs
+++ SampleAPI/obj/Debug/net8.0/SampleAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d25dd0a5ec424a230e05d9f8c3f1c274f627d100")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
