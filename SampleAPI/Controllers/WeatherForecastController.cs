052284a3030c865e5c0fb1f166edb05c90fa0763
Dhiraj <dhirajbirnale384@gmail.com>
2025-03-24 23:32:02 +0530

    commit chnages

--- SampleAPI/Controllers/WeatherForecastController.cs
+++ SampleAPI/Controllers/WeatherForecastController.cs
@@ -26,10 +26,6 @@ public class WeatherForecastController : ControllerBase
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            fdjf
-
-
-            dsfsdjfh 'Dhiraj' bir
         })
         .ToArray();
     }
